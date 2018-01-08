dbcd35cf86ba7be808bfc4f2f42da251ec33bcbc
shshshdy <shshshdy@163.com>
2018-01-08 21:43:29 +0800

    添加项目文件。

---
+++ CommonSdk/ConfigurationManager.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace CommonSdk
+{
+    /// <summary>
+    /// 项目目录下增加app.json做为配置文件
+    /// </summary>
+    public class ConfigurationManager
+    {
+        private static IConfigurationRoot config = null;
+        static ConfigurationManager()
+        {
+            // Microsoft.Extensions.Configuration扩展包提供的
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("app.json");
+            config = builder.Build();
+        }
+
+        public static IConfigurationRoot AppSettings
+        {
+            get
+            {
+                return config;
+            }
+        }
+
+        public static string Get(string key)
+        {
+            return config[key];
+        }
+
+    }
+}
